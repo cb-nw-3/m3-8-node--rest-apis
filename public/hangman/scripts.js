f59a9fef733a951711f9db7575686836f7566854
Vitor Correia <vitorbaziliogcorreia@gmail.com>
2020-06-29 21:05:22 -0400

    Exercise 2 and 3

--- public/hangman/scripts.js
+++ public/hangman/scripts.js
@@ -1,1 +1,117 @@
-// ... crickets...
+// const words = [
+//     "test",
+//     "testing"
+// ]
+
+let maxWrong = 5;
+let mistakes = 0;
+let letterselected = [];
+let wordId = undefined;
+
+//return a random word
+async function getWord() {
+    try {
+        const res = await fetch("/hangman/words");
+        // console.log(res.json());
+        const data = await res.json();
+        return data;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function getAnswer() {
+    try {
+        const res = await fetch(`/hangman/answer/word/${wordId}`);
+        // console.log(res.json());
+        const data = await res.json();
+        return data;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+//Display each letter
+getWord()
+    .then((response) => {
+        console.log(response)
+        wordId = response.id;
+
+        document.getElementById('word').innerHTML = "_ ".repeat(response.length);
+    })
+console.log(getWord())
+
+
+async function getLetter(id, letter) {
+    try {
+        const res = await fetch(`/hangman/guess/${id}/${letter}`);
+        return res.json();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+//Display max mistakes number
+document.getElementById('maxWrong').innerHTML = maxWrong;
+
+//Add buttons
+function buttons() {
+    let displayButtons = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letters =>
+        `
+        <button
+            id='` + letters + `'
+            onClick="handleGuess('` + letters + `')"
+        > 
+            ` + letters + `
+
+        </button>
+        `
+    ).join('');
+    document.getElementById('letters').innerHTML = displayButtons;
+}
+
+
+function handleGuess(chosenLetter) {
+    letterselected.indexOf(chosenLetter) === -1 ? letterselected.push(chosenLetter) : null;
+    document.getElementById(chosenLetter).setAttribute('disable', true);
+
+    if (wordId.indexOf(chosenLetter) >= 0) {
+        guessedWord();
+        checkIfWon();
+    } else if (wordId.indexOf(chosenLetter) === -1) {
+        mistakes++;
+        updateMistakes();
+        checkIfLost();
+    }
+}
+
+async function checkIfWon() {
+    const data = await getAnswer()
+    if (wordStatus === data.word) {
+        document.getElementById('letters').innerHTML = 'You Won!!!'
+    }
+}
+async function checkIfLost() {
+    if (mistakes === maxWrong) {
+        // getAnswer()
+        // .then(asnwer => )
+        const data = await getAnswer()
+        document.getElementById('word').innerHTML = data.word
+        document.getElementById('letters').innerHTML = 'You Lost!!!'
+    }
+}
+
+function updateMistakes() {
+    document.getElementById('mistakes').innerHTML = mistakes;
+}
+
+function reset() {
+    mistakes = 0;
+    letterselected = [];
+
+    buttons();
+}
+
+buttons();
+reset();
+
--- server.js
+++ server.js
@@ -6,17 +6,111 @@ const morgan = require('morgan');
 
 const PORT = process.env.PORT || 8000;
 
+//EXERCISE 2
+//Search a client by ID
+const { clients } = require('./data/clients');
+const handleClient = (req, res) => {
+    let clientID = req.params.id;
+
+    let clientSelected = clients.find((client) => client.id === clientID);
+
+    if (clientSelected) {
+        res.status(200).send(clientSelected);
+    } else {
+        res.status(400).send({ status: 'error', error: 'not found' });
+    }
+}
+//END EXERCISE 2
+
+//EXERCISE 3
+const { words } = require('./data/words');
+const handleWord = (req, res) => {
+    let randomValue = words[Math.floor(Math.random() * words.length)];
+
+    let selectedWord = { id: randomValue.id, length: randomValue.letterCount };
+
+    if (selectedWord) {
+        res.status(200).send(selectedWord);
+    } else {
+        res.status(400).send({ status: 'error', error: 'not found' });
+    }
+}
+
+const handleHangman = (req, res) => {
+    let wordId = req.params.wordId;
+    let letter = req.params.letter;
+
+    //Find the word related to the ID
+    let wordSelected = words.find((word) => word.id === wordId);
+
+    //Letters position
+    const lettersPosition = (word, letter) => {
+        let lettersPosition = [];
+        word.foreach((item) => {
+            if (word[item] === letter) {
+                lettersPosition.push(item);
+            }
+        })
+        console.log(lettersPosition);
+        return lettersPosition;
+    };
+
+    if (wordSelected != undefined) {
+        if (wordSelected.word.includes(letter)) {
+            res.status(200).send({
+                status: 'sucess',
+                message: `the letter ${letter} is included`,
+                position: lettersPosition(wordSelected.word, letter),
+            });
+        } else {
+            //Letter is not included in the selected word
+            res.status(200).send({
+                status: 'fail',
+                message: `the letter ${letter} is NOT included`,
+                position: 'none',
+            });
+        }
+    } else {
+        res.status(400).send({ status: 'error', error: 'ID was not found' });
+    }
+}
+
+const handleAnswer = (req, res) => {
+    console.log("getting here")
+    let answer = words.find(word => word.id === req.params.id);
+
+    if (answer) {
+        res.status(200).send(answer);
+    } else {
+        res.status(400).send({ status: 'error', error: 'not found' });
+    }
+}
+
+//END EXERCISE 3
+
 express()
-    .use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    .use(function (req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
         next();
     })
-	.use(morgan('tiny'))
-	.use(express.static('public'))
+    .use(morgan('tiny'))
+    .use(express.static('public'))
     .use(bodyParser.json())
-    .use(express.urlencoded({extended: false}))
+    .use(express.urlencoded({ extended: false }))
 
     // endpoints
+    //EXERCISE 2
+    .get('/client/:id', handleClient)
+
+    //EXERCISE 3
+    .get('/hangman/words', handleWord)
+
+    .get('/hangman/guess/:wordId/:letter', handleHangman)
+    .get('/hangman/answer/word/:id', handleAnswer)
+
+    .get('*', (req, res) => {
+        res.status(404).send('Error 404: Page not Found');
+    })
 
     .listen(PORT, () => console.log(`Listening on port ${PORT}`));
