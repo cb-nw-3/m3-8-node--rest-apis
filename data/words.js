e3e67fe709e55bbd8dfb7393062f551bd270f738
jenny ma <jenny_lau_8@hotmail.com>
2020-06-30 20:58:36 -0400

    10 words

---
+++ data/words.js
@@ -0,0 +1,44 @@
+const words = [
+    { 
+        word: "earth",
+        id: "1",
+        letterCount: "5" },
+    { 
+        word: "saturn",
+        id: "2",
+        letterCount: "6" },
+    { 
+        word: "mercury",
+        id: "3",
+        letterCount: "7" },
+    { 
+        word: "mars",
+        id: "4",
+        letterCount: "4" },
+    { 
+        word: "venus",
+        id: "5",
+        letterCount: "5" },
+    { 
+        word: "jupiter",
+        id: "6",
+        letterCount: "7" },
+    { 
+        word: "uranus",
+        id: "7",
+        letterCount: "6" },
+    { 
+        word: "neptune",
+        id: "8",
+        letterCount: "7" },
+    { 
+        word: "pluto",
+        id: "9",
+        letterCount: "5" },
+    {
+         word: "moon",
+         id: "10",
+         letterCount: "4" },
+  ];
+  
+module.exports = { words };
