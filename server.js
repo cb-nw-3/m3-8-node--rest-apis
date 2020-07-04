a49c44056ee2ba80a7fad42ae37ad5e1993bd676
Philppe <psalvail@assetscience.com>
2020-07-04 13:14:06 -0400

    completed exercise 2 optional

--- server.js
+++ server.js
@@ -4,7 +4,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const { words } = require("./data/words");
+const { clients } = require("./data/clients");
 let wordSelected = "";
+
+const customerID = (req, res) => {
+  const found = clients.find((customer) => customer.id == req.params.id);
+  if (found) {
+    res.status(200).send({ found });
+  } else {
+    res.status(404).send({ error: "ID not found" });
+  }
+};
 
 const selectWord = (req, res) => {
   let numSelected = Math.floor(Math.random() * words.length);
@@ -40,5 +50,6 @@ express()
   .use(express.urlencoded({ extended: false }))
   .get("/hangman/words", selectWord)
   .get("/hangman/guess/:wordID/:letter", selectLetter)
+  .get("/restaurant/customerProfile/:id", customerID)
 
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
