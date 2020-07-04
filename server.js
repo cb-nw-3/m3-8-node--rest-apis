c2cc260d889c011f6dc533ce72368e956f35cb16
kolbymckeown <mckeownkolby@gmail.com>
2020-07-04 12:10:59 -0400

    hangman complete

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
+// route imports
+const handleWords = require("./handlers/words");
+const handleGuess = require("./handlers/guess");
+
+console.log(handleGuess);
 const PORT = process.env.PORT || 8000;
 
 express()
-    .use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    })
-	.use(morgan('tiny'))
-	.use(express.static('public'))
-    .use(bodyParser.json())
-    .use(express.urlencoded({extended: false}))
+	.use(function (req, res, next) {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header(
+			"Access-Control-Allow-Headers",
+			"Origin, X-Requested-With, Content-Type, Accept"
+		);
+		next();
+	})
+	.use(morgan("tiny"))
+	.use(express.static("public"))
+	.use(bodyParser.json())
+	.use(express.urlencoded({ extended: false }))
+	.get("/hangman/words", handleWords)
+	.get("/hangman/guess/:wordId/:letter", handleGuess)
+	// endpoints
 
-    // endpoints
-
-    .listen(PORT, () => console.log(`Listening on port ${PORT}`));
+	.listen(PORT, () => console.log(`Listening on port ${PORT}`));
