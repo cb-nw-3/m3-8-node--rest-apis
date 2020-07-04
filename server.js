cd4034e7301287459ca7a9eae84fab168ab38851
Etienne Giroux <etienneg@Etiennes-MacBook-Pro.local>
2020-07-04 11:06:59 -0400

    Create serverhandlers for cleaner server.js

--- server.js
+++ server.js
@@ -4,24 +4,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 
-const { clients } = require("./data/clients");
 const { wordObjectsArray } = require("./data/wordBank");
 const { masterKey } = require("./masterKey");
+const {
+  handleUserRequest,
+  handleRandomWordRequest,
+  handleAdminWord,
+  handlePlayerGuess,
+} = require("./serverHandlers");
 
 //console.log(wordObjectsArray[42]);
-
-const handleUserRequest = (req, res) => {
-  console.log("req.body: ", req.body);
-  const { userId } = req.body;
-
-  let requestedUser = clients.find((user) => user.id === userId);
-
-  if (requestedUser) {
-    res.status(200).send({ status: "success", user: requestedUser });
-  } else {
-    res.status(400).send({ status: "error", error: "not found" });
-  }
-};
 
 const PORT = process.env.PORT || 8000;
 
@@ -42,59 +34,10 @@ express()
   // endpoints
   .post("/getUser", handleUserRequest)
 
-  .get("/hangman/words/", (req, res) => {
-    let randomIndex = Math.floor(
-      Math.random() * Math.floor(wordObjectsArray.length)
-    );
-    let word = wordObjectsArray[randomIndex];
+  .get("/hangman/words/", handleRandomWordRequest)
 
-    if (!word) {
-      res.status(500).send({ status: "error", error: "word doesn't exist" });
-    }
+  .get("/hangman/words/:wordId/:key", handleAdminWord)
 
-    res.status(200).send({ id: word.id, letterCount: word.letterCount });
-  })
-
-  .get("/hangman/words/:wordId/:key", (req, res) => {
-    const { key, wordId } = req.params;
-    const word = wordObjectsArray[wordId];
-
-    if (!word) {
-      res.status(404).send({ status: "error", error: "invalid wordId" });
-      return;
-    }
-    if (key === masterKey) {
-      res.status(200).send({ word });
-    } else {
-      res.status(400).send({ error: "not authorised" });
-    }
-  })
-
-  .get("/hangman/guess/:wordId/:letter", (req, res) => {
-    const { wordId, letter } = req.params;
-
-    const secretWord = wordObjectsArray.find(
-      (item) => item.id === Number(wordId)
-    );
-
-    if (!secretWord) {
-      res.status(404).send({ status: "error", error: "invalid wordId" });
-      return;
-    }
-    const wordArray = secretWord.word.split("");
-    const letterPosition = wordArray.map((item) => {
-      return item === letter;
-    });
-    //console.log(letter);
-    if (secretWord.word.includes(letter)) {
-      res
-        .status(200)
-        .send({ status: "included", letterPosition: letterPosition });
-    } else {
-      res
-        .status(200)
-        .send({ status: "not included", letterPosition: letterPosition });
-    }
-  })
+  .get("/hangman/guess/:wordId/:letter", handlePlayerGuess)
 
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
