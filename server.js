da84e27e399ae3303a2bb3ef088872861a130e38
Hiba Z <hiba4.z@gmail.com>
2020-07-08 21:06:09 -0400

    WORKSHOP M3-8 DONE

--- server.js
+++ server.js
@@ -1,22 +1,57 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
+const handleWords = require("./handlers/words");
+const handleGuess = require("./handlers/guess");
 const PORT = process.env.PORT || 8000;
+const words = require("./data/words");
+console.log(words);
+// establish requirements
 
-express()
-    .use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    })
-	.use(morgan('tiny'))
-	.use(express.static('public'))
-    .use(bodyParser.json())
-    .use(express.urlencoded({extended: false}))
+let { clients } = require("./data/clients.js");
+let app = express()
+  .use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
 
-    // endpoints
+    next();
+  })
+  .use(morgan("tiny"))
+  .use(express.static("public"))
+  .use(bodyParser.json())
+  .use(express.urlencoded({ extended: false }))
+  .get("/hangman/words", handleWords)
+  .get("/hangman/guess/:wordId/:letter", handleGuess)
+  //added this
 
-    .listen(PORT, () => console.log(`Listening on port ${PORT}`));
+  .set("view engine", "ejs");
+
+// endpoints
+
+app.get("/client/:id", (req, res) => {
+  const clientSearch = req.params.id;
+
+  // client search will be equal to request by id
+
+  const clientFound = clients.find((client) => {
+    return client.id === clientSearch;
+
+    //take one (client) and then executing function on it. Will return whatever returns true)
+  });
+  if (clientFound != undefined) {
+    let response = { clientFound: clientFound };
+    res.send(response);
+  }
+});
+app.get("*", (req, res) => res.send("Dang. 404."));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+//     There is a `clients.js` file inside of the data folder.
+// HTTP GET http://www.appdomain.com/users
+// Create RESTful endpoint (only one) that expects a search query and responds with the requested data, and/or the appropirate HTTP code.
